02bfb2febf001348d2e8816c64d0cee224c2a213
Sandeepbhagavan <envsandeepbhagavan@gmail.com>
2024-02-26 22:15:32 +0530

    Devtools

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Login from './Login'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Browse from './Browse'
-//import { getAuth, onAuthStateChanged } from "firebase/auth";
-//import { useDispatch } from 'react-redux';
-//import { addUser, removeUser } from '../utils/userSlice';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+import { useEffect } from 'react';
 
 const Body = () => {
-    //const dispath=useDispatch();
+    const dispath=useDispatch();
     const appRouter=createBrowserRouter([
         {
             path:"/",
@@ -18,19 +19,19 @@ const Body = () => {
             element:<Browse/>
         }
     ])
-//     useEffect(()=>{
+    useEffect(()=>{
         
 
-// const auth = getAuth();
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     const uid = user.uid;
-//     dispath(addUser({uid:uid}))
-//   } else {
-//     dispath(removeUser())
-//   }
-// });
-    // },[])
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const {uid,email,displayName} = user;
+    dispath(addUser({uid:uid,email:email,displayName:displayName}))
+  } else {
+    dispath(removeUser())
+  }
+});
+    },[])
   return (
     <div>
       <RouterProvider router={appRouter}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from "firebase/auth";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
     const navigate=useNavigate();
+    const user=useSelector((store)=>store.user)
     const signOutButton=()=>{
-        navigate("/")
+
+const auth = getAuth();
+signOut(auth).then(() => {
+    navigate("/")
+}).catch((error) => {
+  navigate("/error")
+});
+        
     }
   return (
     
     <div className='absolute bg-gradient-to-b w-screen from-black z-10 flex justify-between'>
       <img className="w-44 "src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo'/>
     
-    <div className='flex'>
+    {user && <div className='flex'>
         <button className='text-white p-3 h-10 font-bold bg-red-600' onClick={signOutButton}>Sign Out</button>
-    </div>
+    </div>}
     </div>
     
   )
